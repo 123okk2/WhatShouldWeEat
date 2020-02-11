4e2c4263cdaea52c526941c919192775ae7a9a7f
MinWoo Lee <51351974+123okk2@users.noreply.github.com>
2020-02-11 12:52:13 +0900

    Add files via upload

--- WebContent/resources/JS/inputTextFunction.js
+++ WebContent/resources/JS/inputTextFunction.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /**
  * 
  */
@@ -48,3 +49,55 @@ function checkInput() {
 		}
 	}
 }
+=======
+/**
+ * 
+ */
+
+var num = 2;
+var foodArr = new Array(2);
+foodArr[0] = '', foodArr[1] = '';
+
+function saveFood(i, food) {
+    foodArr[i] = food;
+}
+
+function changeBox(type) {
+    if (type) {
+        num++;
+        foodArr.push("");
+    }
+    else {
+        if (num>2) {
+            num--;
+            foodArr.pop();
+        }
+        else {
+            alert("최소 두 개의 메뉴를 입력해주세요!")
+        }
+    }
+    
+    refreshExplore();
+}
+
+function refreshExplore() {
+    document.getElementById('textBoxes').innerHTML="";
+    document.getElementById('textBoxes').innerHTML += 
+    	"<input type = 'hidden' name = 'readNumber' value="+num+">"
+    for(i=0; i<num; i++) {
+        document.getElementById('textBoxes').innerHTML += 
+            "<input type = 'text' id='" + i + "' name = 'food" + i + "' onchange='saveFood("+i+",this.value)' value='" + foodArr[i]+ "' placeholder='음식을 입력하세요.'><br>";
+    }
+}
+
+function checkInput() {
+	for(i=0; i<num;i++) {
+		var str = eval("showResult.food" + i);
+		if(!str.value) {
+			alert("공란이 존재합니다!")
+			str.focus();
+			return false;
+		}
+	}
+}
+>>>>>>> refs/remotes/origin/master
--- WebContent/resources/JS/resultFunction.js
+++ WebContent/resources/JS/resultFunction.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /**
  * 
  */
@@ -29,4 +30,37 @@ function start() {
 
 function writeFood() {
 	
+=======
+/**
+ * 
+ */
+
+var foodArr = new Array();
+
+function addFood(food) {
+	foodArr.push(food);
+}
+
+function start() {
+	var num = 0;
+	var randArr = new Array();
+	for(i = 0; i < foodArr.length*5; i++) {
+		randArr[i] = Math.round(Math.random()*(foodArr.length-1));
+	}
+	console.log(foodArr);
+	console.log(randArr);
+	var startInterval = setInterval(function() {
+	    document.getElementById('results').value = foodArr[randArr[num]];
+	    console.log(randArr[num] + " " +foodArr[randArr[num]]);
+	    num++;
+	}, 300)
+	setTimeout(function() {
+		clearInterval(startInterval)
+		alert("오늘 메뉴는 " + foodArr[randArr[num-1]] + "입니다!")
+	}, foodArr.length*1500+299);
+}
+
+function writeFood() {
+	
+>>>>>>> refs/remotes/origin/master
 }
